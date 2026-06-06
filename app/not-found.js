f634e8cd50655d1b6ae9603c89593ffd71b8a240
emfkrhwhem1 <emfkrhwhem1@naver.com>
2026-06-07 06:10:04 +0900

    Meals 중도 저장

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>
+                Not Found
+            </h1>
+            <p>Unforunately, we could not find the requested page or Resource</p>
+        </main>
+    )
+}
